409a96541c8d0f9714e37a2e7c2b2cc780fc75e4
Liang Zhang <liang@leon-MacBookAir>
2024-10-30 11:39:46 -0700

    First edition of the simple http DB

---
+++ main.js
@@ -0,0 +1,31 @@
+// app.js
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+const map = {
+}
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the Express server!');
+});
+
+app.get('/get', (req, res) => {
+    const { key } = req.query;
+
+    res.json(map[key] || null);
+});
+
+app.post('/set', (req, res) => {
+    const {key, value} = req.body;
+    map[key] = value;
+    res.status(200).json({ status:0, message: "success" });
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
+
